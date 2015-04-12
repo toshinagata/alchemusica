8e4784428136bea25890cd3c8244308561e5a9c3
toshinagata1964 <toshinagata1964@19e2383c-df9d-4eb5-8775-ae43218f5bcb>
2015-04-12 14:10:56 +0000

    Create a zip archive in Deployment build
    git-svn-id: svn+ssh://svn.osdn.jp/svnroot/alchemusica/trunk@46 19e2383c-df9d-4eb5-8775-ae43218f5bcb

---
+++ make_release_package.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+dt=`date +'%y%m%d'`
+(cd build/Deployment; zip -r Alchemusica_$dt.zip Alchemusica.app -x \*.DS_Store)
+# (cd ..; zip -r Alchemusica_src_$dt.zip Alchemusica -x \*.svn/\* -x _\* -x \*/_\* -x \*.DS_Store -x Alchemusica/build\* -x Alchemusica/\*.zip)
+# mv ../Alchemusica_src_$dt.zip ./
+mv build/Deployment/Alchemusica_$dt.zip ./
