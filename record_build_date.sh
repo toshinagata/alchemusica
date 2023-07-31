8c1948699964ffabc7f959da73864b75e6d4cdf0
Toshi Nagata <alchemist.2005@nifty.com>
2023-07-31 23:16:48 +0900

    The revision number now matches the number of commits in GitHub

--- record_build_date.sh
+++ record_build_date.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 echo "last_build = \"`date '+%Y-%m-%d %H:%M:%S %Z'`\"" > lastBuild.txt
-REVISION_INFO=$((`git rev-list HEAD --count`+10))  #  +10 to match old SVN rev
+REVISION_INFO=`git rev-list HEAD --count`
 #  REVISION_INFO=`(cd ..; svn status -v . --depth=empty | awk '{print $1}')`
 echo "svn_revision = $REVISION_INFO" >> lastBuild.txt
